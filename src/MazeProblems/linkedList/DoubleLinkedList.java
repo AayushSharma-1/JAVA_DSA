1e8f2eea90d0621d71f982d8e6aa96b329b417cf
AayushSharma-1 <aayush9119@gmail.com>
2024-09-21 22:05:54 +0530

    added basic CircularLInkedList

--- src/MazeProblems/linkedList/DoubleLinkedList.java
+++ src/MazeProblems/linkedList/DoubleLinkedList.java
@@ -51,6 +51,33 @@ public class DoubleLinkedList {
 
     }
 
+    public Node find(int val){
+        Node node = head;
+        while (node!=null){
+
+            if(node.val == val){
+                return node;
+            }
+            node = node.next;
+        }
+        return null;
+    }
+
+    public void insert(int after, int val){
+        Node p = find(after);
+        if(p == null){
+            System.out.println("does not exist");
+        }
+        Node node = new Node(val);
+        node.next = p.next;
+        p.next = node;
+        node.prev = p;
+        if(node.next != null){
+            node.next.prev = node;
+        }
+    }
+
+
     private class Node{
         int val;
         Node next;
--- src/MazeProblems/linkedList/Main.java
+++ src/MazeProblems/linkedList/Main.java
@@ -15,13 +15,25 @@ public class Main {
 //        list.delete(2);
 ////        list.deleteFirst();
 //        list.display();
-        DoubleLinkedList list = new DoubleLinkedList();
-        list.insertFirst(3);
-        list.insertFirst(2);
-        list.insertFirst(8);
-        list.insertFirst(17);
-        list.insertLast(99);
+//        DoubleLinkedList list = new DoubleLinkedList();
+//        list.insertFirst(3);
+//        list.insertFirst(2);
+//        list.insertFirst(8);
+//        list.insertFirst(17);
+//        list.insertLast(99);
+//        list.insert(8, 56);
 
+        CircularLInkedList list = new CircularLInkedList();
+        list.insert(3);
+        list.insert(23);
+        list.insert(43);
+        list.insert(36);
+        list.insert(83);
+
+
+        list.display();
+
+        list.delete(43);
         list.display();
 
 
